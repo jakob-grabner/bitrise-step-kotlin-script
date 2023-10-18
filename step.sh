e7722c86ecdc5bd84fdea0b80d76e211ec0201fd
Jakob Grabner <jakob.grabner@runtastic.com>
2023-10-18 15:15:34 +0200

    Fix issue

--- step.sh
+++ step.sh
@@ -3,8 +3,8 @@ set +e
 THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 KOTLIN_SCRIPT_FILE="${THIS_SCRIPT_DIR}/script.main.kts"
 
-if [ "${kotlin_script}" == "" ]; then
-    echo " [!] kotlin_script environment var is missing or empty - no Kotlin Script defined!"
+if [ "${kotlin_content}" == "" ]; then
+    echo " [!] kotlin_content environment var is missing or empty - no Kotlin Script defined!"
     exit 1
 fi
 
@@ -14,7 +14,7 @@ fi
 
 set -e
 # Writing kotlin script to file
-echo "${kotlin_script}" > "${KOTLIN_SCRIPT_FILE}"
+echo "${kotlin_content}" > "${KOTLIN_SCRIPT_FILE}"
 set +e
 
 cmd_ex_code=0
